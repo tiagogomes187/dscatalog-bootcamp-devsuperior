d0c91e8a5b95efa72beeaa5c5453a9c27529a2e4
Tiago Gomes <tiagogomes187@gmail.com>
2021-08-28 15:11:04 -0300

    Global state with Constext API

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,19 @@
 import './assets/styles/custom.scss';
 import './App.css';
 import Routes from 'Routes';
+import { AuthContext, AuthContextData } from 'AuthContext';
+import { useState } from 'react';
 
-const App = () => {
-  return <Routes />;
-};
+function App() {
+  const [authContextData, setAuthContextData] = useState<AuthContextData>({
+    authenticated: false,
+  });
+
+  return (
+    <AuthContext.Provider value={{ authContextData, setAuthContextData }}>
+      <Routes />
+    </AuthContext.Provider>
+  );
+}
 
 export default App;
--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,10 +1,11 @@
 import ButtonIcon from 'components/ButtonIcon';
 import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { getAuthData, requestBackendLogin, saveAuthData } from 'util/requests';
-import { useState } from 'react';
+import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
+import { useContext, useState } from 'react';
 
 import './styles.css';
+import { AuthContext } from 'AuthContext';
 
 type FormData = {
   username: string;
@@ -12,6 +13,8 @@ type FormData = {
 };
 
 const Login = () => {
+  const { setAuthContextData } = useContext(AuthContext);
+
   const [hasError, setHasError] = useState(false);
 
   const {
@@ -26,10 +29,11 @@ const Login = () => {
     requestBackendLogin(formData)
       .then((response) => {
         saveAuthData(response.data);
-        const token = getAuthData().access_token;
-        console.log('TOKEN GERADO: ' + token);
         setHasError(false);
-        console.log('SUCESSO!', response);
+        setAuthContextData({
+          authenticated: true,
+          tokenData: getTokenData(),
+        });
         history.push('/admin');
       })
       .catch((error) => {
