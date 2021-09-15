010cbca96fdde10e3ebee885ef1a6219cf47ad00
Tiago Gomes <tiagogomes187@gmail.com>
2021-09-15 20:58:13 -0300

    Delete product, observer pattern

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -4,12 +4,30 @@ import CategoryBadge from 'pages/Admin/Products/CategoryBadge';
 
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
 type Props = {
   product: Product;
+  onDelete: Function;
 };
 
-const ProductCrudCard = ({ product }: Props) => {
+const ProductCrudCard = ({ product, onDelete }: Props) => {
+  const handleDelete = (productId: number) => {
+    if (!window.confirm('Tem certeza que deseja deletar?')) {
+      return;
+    }
+
+    const config: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: `/products/${productId}`,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      onDelete();
+    });
+  };
   return (
     <div className="base-card product-crud-card">
       <div className="product-crud-card-top-container">
@@ -29,7 +47,10 @@ const ProductCrudCard = ({ product }: Props) => {
       </div>
 
       <div className="product-crud-card-buttons-container">
-        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
+        <button
+          onClick={() => handleDelete(product.id)}
+          className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first"
+        >
           EXCLUIR
         </button>
 
