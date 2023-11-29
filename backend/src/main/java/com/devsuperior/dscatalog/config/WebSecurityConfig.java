9caa1f492bf39f145c22baa8d4ae31ff75c1ec59
Tiago Gomes <tiagogomes187@gmail.com>
2023-11-28 23:40:43 -0300

    Ajustei o método configure da Classe 'WebSecurity'

--- backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -29,6 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/actuator/**");
+		web.ignoring().antMatchers("/images/*.png");
 	}
 
 	@Override
